98d4d255139b189ae6e7cf4697a1dcfe003dbda2
AlbertoGruning <alberto.gruning.zen@gmail.com>
2020-11-06 18:57:53 -0300

    cap13-confirm

---
+++ Models/Accounts/ConfirmModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAdvert.Web31.Models.Accounts
+{
+    public class ConfirmModel
+    {
+    }
+}
